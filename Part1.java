cac1b6db8536c0e516e32f119e846b52fb2563e4
henrygyamfi1000 <114543030+henrygyamfi1000@users.noreply.github.com>
2022-09-27 22:01:20 -0400

    Create Part1.java

---
+++ Part1.java
@@ -0,0 +1,12 @@
+// I am Henry Gyamfi. I am a senior studying Computer Science and Mathematics at Marietta College.
+// I possess a unique skill of breaking down complex information into simple terms that can be understood by other members in a group.
+// I am a big data enthusiast and love to work on huge data sets and generating meaningful information.
+// I possess great organizational skills which aids in completing projects on time with the right organizational structure.
+
+public class Part1 {
+ 
+ public static void main(String[] args) {
+  System.out.println("Name: Henry Gyamfi \n Major: Computer Science \n 1st Job Company: Credit Suisse");
+ }
+ 
+}
